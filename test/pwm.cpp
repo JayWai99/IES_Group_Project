9f1e407138d17a7a9fad473d3a775dc49b4f3400
Winfred-UTS <126643909+Winfred-UTS@users.noreply.github.com>
2024-04-12 15:24:11 +1000

    Update pwm.cpp
    Working PWM code.

--- test/pwm.cpp
+++ test/pwm.cpp
@@ -1,24 +1,33 @@
 /* LIBRARIES */
 #include <avr/io.h>         // For Arduino I/O ports
+#include <util/delay.h>     // For delay
+
+/* MACROS */
+#define CLK_FREQ 16000000   // CLK frequency (Hz)
+#define PWM_DUTY 90         // PWM duty cycle (%)(fixed value for test purposes)
+#define PWM_FREQ 65         // PWM frequency (Hz)(fixed value for test purposes)
+
+/* PINS */
+#define PIN_PWM PIND5       // PWM connected to pin 5 via pin D5
+
+/* CONSTANTS */
+const int prescaler[] = {1, 8, 64, 256, 1024};
+
+/* VARIABLES */
+
 
 /* FUNCTIONS */
 void setup_pwm(void);
-
-/* MACROS */
-#define PWM_DUTY 50         // PWM duty cycle (%)(fixed value for test purposes)
-#define PWM_FREQ 10         // PWM frequency (Hz)(fixed value for test purposes)
-
-/* PINS */
-#define PIN_PWM PINB2       // PWM connected to pin 10 via pin B2
 
 /* MAIN */
 int main(void)
 {
     setup_pwm();
 
-
     while(1)
     {
+        //
+        //_delay_ms(5000);
         //
     }
 }
@@ -30,9 +39,8 @@ void setup_pwm(void)
     float period_on  = period *    (PWM_DUTY/100) ;
     float period_off = period * (1-(PWM_DUTY/100));
 
-    // PWM pin to output mode and initialise low
-    DDRB   |=  (1 << PIN_PWM);
-    PORTB  &= ~(1 << PIN_PWM);
+    // PWM pin to output mode
+    DDRD   |=  (1 << PIN_PWM);
 
     // Timer/counter to fast PWM MAX
     // Requires setting WGMx to 011
@@ -45,24 +53,45 @@ void setup_pwm(void)
 
     // Timer/counter to fast PWM TOP
     // Requires setting WGMx[0-2] to 111
-    TCCR1B |=  (1 << WGM12);
-    TCCR1A |=  (1 << WGM11);
-    TCCR1A |=  (1 << WGM10);
+    TCCR0B |=  (1 << WGM02);
+    TCCR0A |=  (1 << WGM01);
+    TCCR0A |=  (1 << WGM00);
 
     // Output compare to toggle OCxA on compare match
-    // Requires setting COMxA[0-1] to 01 
-    TCCR1A &= ~(1 << COM1A1);
-    TCCR1A |=  (1 << COM1A0);
+    // Requires setting COMxA[0-1] to 01
+    TCCR0A &= ~(1 << COM0A1);
+    TCCR0A |=  (1 << COM0A0);
     // Output compare to non-inverting mode
     // Requires setting COMxB[0-1] to 10
-    TCCR1A |=  (1 << COM1B1);
-    TCCR1A &= ~(1 << COM1B0);
+    TCCR0A |=  (1 << COM0B1);
+    TCCR0A &= ~(1 << COM0B0);
 
     // Select prescaler for
     // Prescaler for 
 
+    // Clock source to F_CLK / 1024 from prescaler 
+    // Requires setting CSx[0-2] to 101
+    TCCR0B |=  (1 << CS02);
+    TCCR0B &= ~(1 << CS01);
+    TCCR0B |=  (1 << CS00);
+
+    // for (size_t i = 0; i < count; i++)
+    // {
+    //     /* code */
+    // }
+    
+
     // Define TOP as PWM period
-    //TCCR1B |=  ();
+
+    // OCR0A defines TOP value
+    // OCR0B defines duty cycle value
+    OCR0A = 240;
+    OCR0B = 240*(PWM_DUTY/100);
+
+    // OCR0A defines TOP value
+    // OCR0B defines duty cycle value
+    //OCR0A = CLK_FREQ / (PWM_FREQ * );
+    //OCR0B = 1/PWM_FREQ;
 
     // Calculate 
 }
