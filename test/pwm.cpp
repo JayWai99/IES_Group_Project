e0af81776e35f1bd8d2f07807464b774946e1940
Winfred-UTS <winfred.ou@student.uts.edu.au>
2024-04-18 20:31:55 +1000

    Update pwm.cpp
    Working PWM with brightness alteration for testing purposes.

--- test/pwm.cpp
+++ test/pwm.cpp
@@ -1,14 +1,18 @@
 /* LIBRARIES */
 #include <avr/io.h>         // For Arduino I/O ports
+#define CLK_FREQ 16000000   // CLK frequency (Hz)
+#define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>     // For delay
+#include "usart.h"
 
 /* MACROS */
-#define CLK_FREQ 16000000   // CLK frequency (Hz)
-#define PWM_DUTY 90         // PWM duty cycle (%)(fixed value for test purposes)
-#define PWM_FREQ 65         // PWM frequency (Hz)(fixed value for test purposes)
+//#define PWM_DUTY 90         // PWM duty cycle (%)(fixed value for test purposes)
+//#define PWM_FREQ 65         // PWM frequency (Hz)(fixed value for test purposes)
 
 /* PINS */
-#define PIN_PWM PIND5       // PWM connected to pin 5 via pin D5
+#define PIN_TC0 PIND5       // PWM connected to pin 5 via pin D5
+#define PIN_PWM PIND6       // PWM connected to pin 6 via pin D6
+#define BAUD_RATE 9600      //baud rate
 
 /* CONSTANTS */
 const int prescaler[] = {1, 8, 64, 256, 1024};
@@ -18,28 +22,47 @@ const int prescaler[] = {1, 8, 64, 256, 1024};
 
 /* FUNCTIONS */
 void setup_pwm(void);
+void setup_usart(void);
 
 /* MAIN */
 int main(void)
 {
     setup_pwm();
+    setup_usart();
+    _delay_ms(500);
 
     while(1)
     {
-        //
-        //_delay_ms(5000);
-        //
+        // For test purposes, change duty cycle to 25%, then 50%, then 75%
+        //OCR0B = OCR0A*(0/100);
+        //_delay_ms(1000);
+        float top = 240;
+        OCR0B = top*((float)25/(float)100);
+        usart_teleplot("OCR0A", OCR0A);
+        usart_teleplot("OCR0B", OCR0B);
+        _delay_ms(1000);
+        OCR0B = top*((float)50/(float)100);
+        usart_teleplot("OCR0A", OCR0A);
+        usart_teleplot("OCR0B", OCR0B);
+        _delay_ms(1000);
+        OCR0B = top*((float)75/(float)100);
+        usart_teleplot("OCR0A", OCR0A);
+        usart_teleplot("OCR0B", OCR0B);
+        _delay_ms(1000);
+        //OCR0B = OCR0A*(100/100);
+        //_delay_ms(1000);
     }
 }
 
 void setup_pwm(void)
 {
     // Calculate PWM parameters
-    float period = 1/PWM_FREQ;
-    float period_on  = period *    (PWM_DUTY/100) ;
-    float period_off = period * (1-(PWM_DUTY/100));
+    // float period = 1/PWM_FREQ;
+    // float period_on  = period *    (PWM_DUTY/100) ;
+    // float period_off = period * (1-(PWM_DUTY/100));
 
-    // PWM pin to output mode
+    // T/C0 pins to output mode
+    DDRD   |=  (1 << PIN_TC0);
     DDRD   |=  (1 << PIN_PWM);
 
     // Timer/counter to fast PWM MAX
@@ -69,11 +92,13 @@ void setup_pwm(void)
     // Select prescaler for
     // Prescaler for 
 
-    // Clock source to F_CLK / 1024 from prescaler 
+    // Clock source to F_CLK / 1024 from prescaler
     // Requires setting CSx[0-2] to 101
+    // Clock source to F_CLK / 256 from prescaler for test
+    // Requires setting CSx[0-2] to 100
     TCCR0B |=  (1 << CS02);
     TCCR0B &= ~(1 << CS01);
-    TCCR0B |=  (1 << CS00);
+    TCCR0B &= ~(1 << CS01);
 
     // for (size_t i = 0; i < count; i++)
     // {
@@ -82,16 +107,21 @@ void setup_pwm(void)
     
 
     // Define TOP as PWM period
-
+    int top = 240;
     // OCR0A defines TOP value
     // OCR0B defines duty cycle value
-    OCR0A = 240;
-    OCR0B = 240*(PWM_DUTY/100);
+    OCR0A = top;
+    //OCR0B = top*(PWM_DUTY/100);
 
     // OCR0A defines TOP value
     // OCR0B defines duty cycle value
     //OCR0A = CLK_FREQ / (PWM_FREQ * );
     //OCR0B = 1/PWM_FREQ;
 
-    // Calculate 
+    // Calculate
+}
+
+void setup_usart(void)
+{
+    usart_init(usart_ubrr(F_CPU, BAUD_RATE));
 }
