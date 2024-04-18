d3e69b116080e2f24e010c31507af0eef593169c
Winfred-UTS <winfred.ou@student.uts.edu.au>
2024-04-18 20:43:02 +1000

    Update led.cpp
    Tested LED brightness code.

--- src/led.cpp
+++ src/led.cpp
@@ -19,7 +19,7 @@
 #define PIN_LED PIND5       // LED connected to pin 5 via pin D5
 
 /* CONSTANTS */
-static const int prescaler[] = {0, 1, 8, 64, 256, 1024};
+static const int prescaler[] = {1, 8, 64, 256, 1024};
 
 /* VARIABLES */
 
@@ -27,14 +27,34 @@ static const int prescaler[] = {0, 1, 8, 64, 256, 1024};
 // This function initialises the LED by setting the associated DDR to output mode
 void setup_led(void)
 {
-    BIT_SET(DDRD, PIN_LED);    // LED pin to output mode
+    // LED pin to output mode
+    DDRD |= (1 << PIN_LED);
+
+    // Timer/counter to fast PWM TOP
+    // Requires setting WGMx[0-2] to 111
+    TCCR0B |=  (1 << WGM02);
+    TCCR0A |=  (1 << WGM01);
+    TCCR0A |=  (1 << WGM00);
+
+    // Output compare to toggle OCxA on compare match
+    // Requires setting COMxA[0-1] to 01
+    TCCR0A &= ~(1 << COM0A1);
+    TCCR0A |=  (1 << COM0A0);
+    // Output compare to non-inverting mode
+    // Requires setting COMxB[0-1] to 10
+    TCCR0A |=  (1 << COM0B1);
+    TCCR0A &= ~(1 << COM0B0);
 }
 
 // This function should set the LED to be 
-void set_status_led(bool enabled) {
-    if (enabled) {
+void set_status_led(bool enabled)
+{
+    if (enabled)
+    {
         // Enable the LED to flash with the current brightness and frequency.
-    } else {
+    }
+    else
+    {
         // Disable the LED from flashing. The current brightness and frequency should not be discarded.
     }
 }
@@ -45,13 +65,13 @@ void set_brightness_led(brightness_t brightness)
     switch (brightness)
     {
         case BRIGHTNESS_LOW:
-            OCR0B = OCR0A*(LED_LO/100);
+            OCR0B = OCR0A*((float)LED_LO/(float)100);
             break;
         case BRIGHTNESS_MEDIUM:
-            OCR0B = OCR0A*(LED_MD/100);
+            OCR0B = OCR0A*((float)LED_MD/(float)100);
             break;
         case BRIGHTNESS_HIGH:
-            OCR0B = OCR0A*(LED_HI/100);
+            OCR0B = OCR0A*((float)LED_HI/(float)100);
             break;
         default: // This should never happen, but it's good to have a default case just in case.
             LOG_DEBUG_VARIABLE("Invalid brightness level", (double) brightness);
