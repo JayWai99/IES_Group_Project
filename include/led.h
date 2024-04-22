658ec4f33b0952c11171a38ac0ecec1ba18df5a3
Winfred-UTS <winfred.ou@student.uts.edu.au>
2024-04-22 21:55:10 +1000

    Update led.h

--- include/led.h
+++ include/led.h
@@ -16,7 +16,6 @@ void set_status_led(bool enabled);
 
 void set_brightness_led(brightness_t brightness);
 
-// This function should set the frequency of the LED.
 // Valid frequencies are between 62 Hz and 60,000 Hz.
 void set_frequency_led(float frequency);
 
