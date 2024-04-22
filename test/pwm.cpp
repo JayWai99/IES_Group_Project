ba02dfa16ca5bee825ba7d767306ab95f70167f7
Winfred-UTS <winfred.ou@student.uts.edu.au>
2024-04-22 22:19:14 +1000

    Update pwm.cpp

--- test/pwm.cpp
+++ test/pwm.cpp
@@ -8,7 +8,8 @@
 #include <stddef.h>         // For size_t
 
 #include "usart.h"
-#include "led.h"
+//#include "led.h"
+#include "buzzer.h"
 
 /* MACROS */
 
@@ -29,30 +30,40 @@ int main(void)
     setup_usart();
     _delay_ms(500);
 
-    setup_led();
+    //setup_led();
+    setup_buzzer();
 
-    set_brightness_led(BRIGHTNESS_LOW);
-    
-    bool led_enabled = true;
-    set_status_led(led_enabled);
+    //set_brightness_led(BRIGHTNESS_LOW);
+    set_volume_buzzer(VOLUME_LOW);
+
+    //bool led_enabled = true;
+    bool buzzer_enabled = true;
+    //set_status_led(led_enabled);
+    set_status_buz(buzzer_enabled);
 
     while(1)
     {
-        for (size_t i = 65; i < 60065; i = i + 10000)
+        for (size_t i = 65; i < 2065; i = i + 100)
         {
-            led_enabled = !led_enabled;
-            set_status_led(led_enabled);
+            // //led_enabled = !led_enabled;
+            // buzzer_enabled = !buzzer_enabled;
+            // //set_status_led(led_enabled);
+            // set_status_buz(buzzer_enabled);
 
-            set_frequency_led(i);
+            //set_frequency_led(i);
+            set_frequency_buzzer(i);
             usart_teleplot("F", i);
 
-            set_brightness_led(BRIGHTNESS_LOW);
+            //set_brightness_led(BRIGHTNESS_LOW);
+            set_volume_buzzer(VOLUME_LOW);
             print_variables();
             _delay_ms(2000);
-            set_brightness_led(BRIGHTNESS_MEDIUM);
+            //set_brightness_led(BRIGHTNESS_MEDIUM);
+            set_volume_buzzer(VOLUME_MEDIUM);
             print_variables();
             _delay_ms(2000);
-            set_brightness_led(BRIGHTNESS_HIGH);
+            //set_brightness_led(BRIGHTNESS_HIGH);
+            set_volume_buzzer(VOLUME_HIGH);
             print_variables();
             _delay_ms(2000);
         }
@@ -63,7 +74,10 @@ void print_variables(void)
 {
     usart_teleplot("OCR0A", OCR0A);
     usart_teleplot("OCR0B", OCR0B);
-    usart_teleplot("CS", (TCCR0B >> CS00) & 0b111);
+    usart_teleplot("CS", (TCCR0B >> CS20) & 0b111);
+    usart_teleplot("OCR2A", OCR2A);
+    usart_teleplot("OCR2B", OCR2B);
+    usart_teleplot("CS", (TCCR2B >> CS20) & 0b111);
 }
 
 void setup_usart(void)
