4777ee16002c21917171f0478921770c07aaad51
JayWai99 <jaydencwk@gmail.com>
2024-05-28 20:06:56 +1000

    Implemented code that uses pin change interrupt.

--- src/power_button.cpp
+++ src/power_button.cpp
@@ -1,21 +1,26 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include "power_button.h"
 #include "bit.h"
 
 #define POWER_BUTTON_PIN PINB3
 #define DEBOUNCE_DELAY 100 
 
-
+bool state = false;
 static bool last_state = false;
+
 // This function should initialize the power button. This should mostly involve initializing the pins and other register values.
 void setup_power_button(void) {
     // Set the push button as output and initialise it to logic high
-    BIT_SET(DDRB, POWER_BUTTON_PIN);
     BIT_SET(PORTB, POWER_BUTTON_PIN);
 
     // Set the push button as input
     BIT_CLEAR(DDRB, POWER_BUTTON_PIN);
+
+    BIT_SET(PCICR, PCIE0);
+
+    BIT_SET(PCMSK0, PCINT3);
 }
 
 
@@ -27,7 +32,6 @@ bool power_button_read(void) {
 // This function should be debounced. If the button status isn't changed from the last known value it should return immediately.
 // Otherwise, it should wait so the output can be debounced (verified) and then return the new button status.
 bool is_pressed_power_button(void) {
-    bool state = power_button_read();
     if (state == last_state) {
         last_state = state;
         return false;
@@ -41,3 +45,9 @@ bool is_pressed_power_button(void) {
     return false;
 }
 
+
+ISR(PCINT0_vect){
+  if (power_button_read()){
+      state = !state;  
+    }
+}
