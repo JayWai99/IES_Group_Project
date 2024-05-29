9dca4b0f200c8af850eec7492fe4093b3ba9f9b4
Winfred-UTS <winfred.ou@student.uts.edu.au>
2024-05-29 15:27:25 +1000

    LED and Photoresistor
    Tested LED and photoresistor functions.
    
    Use in main loop:
    set_status_led(read_status_photoresistor());

--- include/led.h
+++ include/led.h
@@ -1,22 +1,7 @@
 #ifndef LED_H
 #define LED_H
 
-#include <stdbool.h>
-
-typedef enum {
-    BRIGHTNESS_LOW = 1,
-    BRIGHTNESS_MEDIUM,
-    BRIGHTNESS_HIGH
-} brightness_t;
-
-// LED starts with T/C0 output compare module in non-inverting fast PWM mode by default
 void setup_led(void);
-
 void set_status_led(bool enabled);
-
-void set_brightness_led(brightness_t brightness);
-
-// Valid frequencies are between 62 Hz and 60,000 Hz.
-void set_frequency_led(float frequency);
 
 #endif // LED_H
--- src/photoresistor.cpp
+++ src/photoresistor.cpp
@@ -1,124 +1,35 @@
+/* HEADER */
 #include "photoresistor.h"
-#include "bit.h"
 
-#include <stdint.h>
-#include <avr/interrupt.h>
+/* LIBRARIES */
+#include <avr/io.h>         // For Arduino I/O ports
+#include <avr/interrupt.h>  // For Arduino interrupts
+#include <util/delay.h>     // For Utility delay
+#include <stddef.h>         // For size_t
 
-#define PHOTORESISTOR_PIN PINC0
-#define PHOTORESISTOR_CHANNEL ADC_CHANNEL_0
+#include "led.h"            // For LED
 
-typedef enum {
-    ADC_REFERENCE_VOLTAGE_AREF = 0b00,
-    ADC_REFERENCE_VOLTAGE_AVCC = 0b01,
-    ADC_REFERENCE_VOLTAGE_INTERNAL = 0b11
-} adc_reference_voltage_t;
+#include "usart.h"          // For USART functions
 
-void set_adc_reference_voltage(adc_reference_voltage_t reference_voltage) {
-    ADMUX |= (reference_voltage & 0b11) << REFS0;
+/* PINS */
+#define PIN_AC_POS PIND6       // AN0 connected to pin  6 via pin C6
+#define PIN_AC_NEG PIND7       // AN1 connected to pin  7 via pin C7
+
+/* CONTENTS */
+void setup_photoresistor(void)
+{
+    // AC pins input mode
+    DDRD   &= ~(1 << PIN_AC_POS);
+    DDRD   &= ~(1 << PIN_AC_NEG);
+
+    // Analogue comparator positive input AIN0 PIND6
+    ACSR   &= ~(1 << ACBG);
+    
+    // Analogue comparator negative input AIN1 PIND7
+    ADCSRB &= ~(1 << ACME);
 }
 
-typedef enum {
-    ADC_CHANNEL_0 = 0b0000,
-    ADC_CHANNEL_1 = 0b0001,
-    ADC_CHANNEL_2 = 0b0010,
-    ADC_CHANNEL_3 = 0b0011,
-    ADC_CHANNEL_4 = 0b0100,
-    ADC_CHANNEL_5 = 0b0101,
-    ADC_CHANNEL_6 = 0b0110,
-    ADC_CHANNEL_7 = 0b0111,
-    ADC_CHANNEL_8 = 0b1000,
-    ADC_CHANNEL_BANDGAP = 0b1110,
-    ADC_CHANNEL_GND = 0b1111
-} adc_channel_t;
-
-void set_adc_channel(adc_channel_t channel) {
-    ADMUX |= (channel & 0b1111) << MUX0;
-}
-
-void set_adc_left_aligned(bool left_aligned) {
-    BIT_ASSIGN(ADMUX, ADLAR, left_aligned);
-}
-
-typedef enum {
-    ADC_PRESCALER_2 = 0b001,
-    ADC_PRESCALER_4 = 0b010,
-    ADC_PRESCALER_8 = 0b011,
-    ADC_PRESCALER_16 = 0b100,
-    ADC_PRESCALER_32 = 0b101,
-    ADC_PRESCALER_64 = 0b110,
-    ADC_PRESCALER_128 = 0b111
-} adc_prescaler_t;
-
-void set_adc_prescaler(adc_prescaler_t prescaler) {
-    ADCSRA |= (prescaler & 0b111) << ADPS0;
-}
-
-typedef enum {
-    ADC_AUTO_TRIGGER_SOURCE_FREE_RUNNING = 0b000,
-    ADC_AUTO_TRIGGER_SOURCE_ANALOG_COMPARATOR = 0b001,
-    ADC_AUTO_TRIGGER_SOURCE_EXTERNAL_INTERRUPT_REQUEST0 = 0b010,
-    ADC_AUTO_TRIGGER_SOURCE_TIMER_COUNTER0_COMPARE_MATCH_A = 0b011,
-    ADC_AUTO_TRIGGER_SOURCE_TIMER_COUNTER0_OVERFLOW = 0b100,
-    ADC_AUTO_TRIGGER_SOURCE_TIMER_COUNTER_COMPARE_MATCH_B = 0b101,
-    ADC_AUTO_TRIGGER_SOURCE_TIMER_COUNTER1_OVERFLOW = 0b110,
-    ADC_AUTO_TRIGGER_SOURCE_TIMER_COUNTER1_CAPTURE_EVENT = 0b111
-} adc_auto_trigger_source_t;
-
-void set_adc_auto_trigger_source(adc_auto_trigger_source_t auto_trigger_source) {
-    ADCSRB |= (auto_trigger_source & 0b111) << ADTS0;
-}
-
-void set_adc_auto_trigger(bool auto_trigger) {
-    BIT_ASSIGN(ADCSRA, ADATE, auto_trigger);
-}
-
-void set_adc_enabled(bool enabled) {
-    BIT_ASSIGN(ADCSRA, ADEN, enabled);
-}
-
-void set_adc_interrupt(bool enabled) {
-    BIT_ASSIGN(ADCSRA, ADIE, enabled);
-}
-
-void start_adc_conversion(void) {
-    BIT_SET(ADCSRA, ADSC);
-}
-
-void setup_adc(adc_channel_t channel, adc_reference_voltage_t reference_voltage, adc_prescaler_t prescaler, bool interrupt) {
-    set_adc_channel(channel);
-    set_adc_reference_voltage(reference_voltage);
-    set_adc_prescaler(prescaler);
-    set_adc_left_aligned(false);
-    set_adc_auto_trigger(false);
-    set_adc_interrupt(interrupt);
-    set_adc_enabled(true);
-}
-
-void setup_adc_auto_trigger(adc_auto_trigger_source_t auto_trigger_source) {
-    set_adc_auto_trigger(true);
-    set_adc_auto_trigger_source(auto_trigger_source);
-}
-
-static volatile uint16_t photoresistor_value;
-
-ISR(ADC_vect) {
-    // Read the ADC value and save so that the last know value can be read
-    photoresistor_value = ADC;
-}
-
-// This function should initialize the photoresistor. This should mostly involve initializing the pins and other register values.
-void setup_photoresistor(void) {
-    photoresistor_value = 0;
-
-    BIT_CLEAR(DDRC, PHOTORESISTOR_PIN); // Set the photoresistor pin as input
-
-    setup_adc(PHOTORESISTOR_CHANNEL, ADC_REFERENCE_VOLTAGE_AVCC, ADC_PRESCALER_128, true);
-    setup_adc_auto_trigger(ADC_AUTO_TRIGGER_SOURCE_FREE_RUNNING);
-    sei();
-    start_adc_conversion();
-}
-
-// This function should read the current value of the photoresistor and return it, scaled to a value between 0 and 1. Where 0 is the darkest and 1 is the brightest.
-float read_photoresistor(void) {
-    return (float) photoresistor_value / 1023.0f;
+bool read_status_photoresistor(void)
+{
+    return (1 & (ACSR >> ACO));
 }
